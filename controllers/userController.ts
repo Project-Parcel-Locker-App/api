c4e77712f52c8c8897e96531941a45efdaf94e0c
t2haev00 <eveliinahampus@gmail.com>
2023-11-11 23:41:59 +0200

    feat: authentication

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { Secret } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export class UserController {
   // Existing method to generate access token
   static generateAccessToken(user: any) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET as Secret, { expiresIn: '15m' });
   }
 
   // Existing method to generate refresh token
   static generateRefreshToken(user: any) {
-    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET as Secret);
   }
 
   static registerUser(req: Request, res: Response) {
@@ -18,7 +21,7 @@ export class UserController {
 
     // Your registration logic to save the user to the database
 
-    const user = { username, email }; // Mock user for token generation
+    const user = { username, email, password }; // Mock user for token generation
     const accessToken = UserController.generateAccessToken(user);
     const refreshToken = UserController.generateRefreshToken(user);
 
@@ -31,17 +34,19 @@ export class UserController {
 
     // Your authentication logic to validate the user
 
-    const user = { email }; // Mock user for token generation
+    const user = { email, password }; // Mock user for token generation
     const accessToken = UserController.generateAccessToken(user);
     const refreshToken = UserController.generateRefreshToken(user);
 
     res.json({ accessToken, refreshToken });
   }
 
-  // ! UNDERSCORE !
-  static getUserInfo(_req: Request, res: Response) {
+  // !
+  static getUserInfo(req: Request, res: Response) {
     // Get user information based on authentication
-    const userProfile = { /* retrieve user profile based on authentication */ };
+    const userProfile = req.user;
+    //{ /* retrieve user profile based on authentication */};
+
     res.json(userProfile);
   }
 
@@ -50,11 +55,14 @@ export class UserController {
     const refreshToken = req.body.token;
     if (!refreshToken) return res.sendStatus(401);
 
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err: any, user: any) => {
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET as Secret, (err: any, user: any) => {
       if (err) return res.sendStatus(403);
 
       const accessToken = UserController.generateAccessToken({ email: user.email });
       res.json({ accessToken });
+      return;
     });
+
+    return res.sendStatus(500); // Return an error if the function doesn't return earlier
   }
 };
