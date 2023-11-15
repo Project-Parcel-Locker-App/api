31b5b95104950d6090da67432c148248a0279aee
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2023-11-15 12:51:58 +0200

    feat: Create controller to handle 404 error in routes

--- app.ts
+++ app.ts
@@ -1,19 +1,17 @@
-import express, { Request, Response, Application } from 'express';
+import express, { Application } from 'express';
 import cors from 'cors';
+import { handle404Error } from 'utils/handle404Error';
 import userRouter from './routes/userRoutes';
 import parcelRouter from './routes/parcelRoutes';
 import cabinetRouter from './routes/cabinetRoutes';
 
 const app: Application = express();
+app.disable('x-powered-by');
 app.use([
 	cors(),
 	express.json(), // for parsing application/json
 	express.urlencoded({ extended: false }), // for parsing application/x-www-form-urlencoded
 ]);
-
-app.get('/api', (_req: Request, res: Response) => {
-	res.send('Welcome to Express TypeScript Server.');
-});
 
 // Middleware setup, if needed
 // app.use(yourMiddleware);
@@ -22,6 +20,7 @@ app.get('/api', (_req: Request, res: Response) => {
 app.use('/api/users', userRouter);
 app.use('/api/parcels', parcelRouter);
 app.use('/api/cabinets', cabinetRouter);
+app.use('*', handle404Error);
 
 // Start the server
 const PORT = Number(process.env.PORT || 3000);
