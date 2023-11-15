2113283f66261b7bcaf7cde8a1077c738b0b76eb
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2023-11-15 17:46:55 +0200

    feat: Create appRouter for a better routes handling

---
+++ routes/appRouter.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { handle404Error } from 'utils/handle404Error';
+import userRouter from './userRoutes';
+import parcelRouter from './parcelRoutes';
+import cabinetRouter from './cabinetRoutes';
+import lockerRouter from './lockerRoutes';
+
+const appRouter = Router();
+
+appRouter.use('/users', userRouter);
+appRouter.use('/parcels', parcelRouter);
+appRouter.use('/cabinets', cabinetRouter);
+appRouter.use('/lockers', lockerRouter);
+appRouter.use('*', handle404Error);
+
+export default appRouter;
