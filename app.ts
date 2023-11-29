891cd92407c5c603d475275b28f68b9b3ecd9117
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2023-11-29 12:54:34 +0200

    chore: Remove unused lines of code

--- app.ts
+++ app.ts
@@ -18,24 +18,6 @@ app.use([
 // Routes
 app.use('/api', appRouter);
 
-// Eve's testing
-// Serve static files
-app.use(express.static('testing')); // directory contains your HTML files
-app.post('/login', (_req, res) => {
-	res.sendFile(__dirname + '/testing/login.html');
-});
-app.get('/parcels/history', (_req, res) => {
-	res.sendFile(__dirname + '/testing/parcelhistory.html');
-});
-app.put('/users/userprofile', (_req, res) => {
-	res.sendFile(__dirname + '/testing/userprofile.html');
-});
-app.post('/parcels/send', (_req, res) => {
-	res.sendFile(__dirname + '/testing/sendparcel.html');
-});
-
-export default app;
-
 // Start the server
 const PORT = Number(process.env.PORT ?? 3000);
 app.listen(PORT, () => {
