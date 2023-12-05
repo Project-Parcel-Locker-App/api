42785b40a0761ce59b7c5184e3e865d3217f795f
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2023-12-05 18:49:58 +0200

    feat: Add config for cors and separate application from server logic

--- app.ts
+++ app.ts
@@ -1,25 +1,25 @@
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
 import express, { Application } from 'express';
-import cors from 'cors';
+import { ACCEPTED_ORIGINS } from './configs/corsOrigins.js';
+import { corsMiddleware } from './middleware/cors.js';
 import { appRouter } from './routes/appRouter.js';
 
 const app: Application = express();
-app.disable('x-powered-by');
 
-if (process.env.ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
 	const morgan = await import('morgan');
 	app.use(morgan.default('dev'));
 }
+
+app.disable('x-powered-by');
 app.use([
-	cors(),
+	corsMiddleware(ACCEPTED_ORIGINS, true),
+	cookieParser(),
 	express.json(),
 	express.urlencoded({ extended: false }),
 ]);
 
-// Routes
 app.use('/api', appRouter);
 
-// Start the server
-const PORT = Number(process.env.PORT ?? 3000);
-app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
-});
+export { app };
---
+++ server.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import { app } from './app.js';
+
+const PORT = Number(process.env.PORT ?? 3000);
+
+// Start the server
+app.listen(PORT, () => {
+	console.log(`Server is listening on http://localhost:${PORT}`);
+});
