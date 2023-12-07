039b4cc6456328a5843b82e0049538d956977c98
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2023-12-07 14:02:56 +0200

    chore: Return token as JSON for dev pruposes, update log info with API URL

--- server.ts
+++ server.ts
@@ -5,5 +5,5 @@ const PORT = Number(process.env.PORT ?? 3000);
 
 // Start the server
 app.listen(PORT, () => {
-	console.log(`Server is listening on http://localhost:${PORT}`);
+	console.log(`Server is listening on http://localhost:${PORT}/api`);
 });
