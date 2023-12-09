29dd24e51e355f71cda971f0c9659f3daf974979
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2023-12-09 19:32:55 +0200

    fix: Update function to throw error when lat and log not found

--- utils/geolocation.ts
+++ utils/geolocation.ts
@@ -2,9 +2,8 @@ const getCoordinates = async (
 	street: string,
 	zipCode: number,
 	city: string,
-) => {
+): Promise<{ lat: number | undefined; lon: number | undefined }> => {
 	const nominatimUrl = `https://nominatim.openstreetmap.org/search?q=${street},+${zipCode},+${city}&format=json&polygon=1&addressdetails=1`;
-	// Set application name to avoid getting blocked by Nominatim
 	const nominatimOptions = {
 		headers: {
 			'User-Agent': 'pulssiposti-api-v1',
@@ -12,7 +11,10 @@ const getCoordinates = async (
 	};
 	const nominatimResponse = await fetch(nominatimUrl, nominatimOptions);
 	const nominatimData = await nominatimResponse.json();
-	const { lat, lon } = nominatimData[0];
+	if (!Array.isArray(nominatimData) || nominatimData.length === 0) {
+		return { lat: undefined, lon: undefined };
+	}
+	const { lat, lon }: { lat: number; lon: number } = nominatimData[0];
 	return { lat, lon };
 };
 
