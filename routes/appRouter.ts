34ec3ac8d640a09b23e3c795707db0cf9194a711
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2023-12-11 17:52:42 +0200

    fix: Update imports for producction

--- routes/appRouter.ts
+++ routes/appRouter.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
 // import { parcelRouter } from './parcelRoutes.js';
-import { authenticateToken } from 'middleware/authorization.js';
+import { authenticateToken } from '../middleware/authorization.js';
 import { handle404Error } from '../middleware/handle404Error.js';
 import { authRouter } from './authRouter.js';
-import cabinetRouter from './cabinetRoutes.js';
 import { lockerRouter } from './lockerRoutes.js';
 import { userRouter } from './userRoutes.js';
 
@@ -12,7 +11,6 @@ const appRouter = Router();
 appRouter.use('/auth', authRouter);
 appRouter.use('/users', authenticateToken, userRouter);
 // appRouter.use('/parcels', authenticateToken, parcelRouter);
-appRouter.use('/cabinets', cabinetRouter);
 appRouter.use('/lockers', lockerRouter);
 appRouter.use('*', handle404Error);
 
