8d5155bcf2d37d6122dd2f6bddb9dd21aa4875e4
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2023-12-12 23:07:42 +0200

    refactor: Update model and controller to retrieve parcel by pickup or sending code

--- models/parcels.ts
+++ models/parcels.ts
@@ -14,33 +14,51 @@ const create = async (
 		parcel.parcel_weight,
 		parcel.special_instructions || null,
 		parcel.parcel_size,
-		// parcel.destination_locker_id,
 		senderId,
 		recipientId,
 	]);
 	return result.rows[0] || null;
 };
 
-const getBySendingCode = async (
-	sendingCode: string,
-): Promise<Parcel | null> => {
-	const query = 'SELECT id from parcels WHERE sending_code = $1';
-	const result: QueryResult<Parcel> = await pool.query(query, [sendingCode]);
-	
+const getParcelByCode = async (code: string | null): Promise<Parcel | null> => {
+	const query = `
+		SELECT 
+			cabinets.locker_id, cabinets.id AS cabinet_id, parcels.id AS parcel_id, sending_code, pickup_code, parcel_weight, special_instructions, parcel_size,
+			CONCAT(sender.first_name,' ',sender.last_name) AS sender_full_name, CONCAT(recipient.first_name,' ',recipient.last_name) AS recipient_full_name,
+			parcel_status, ready_for_pickup_at, parcels.updated_at, parcels.created_at, row_to_json(locations) AS locker_address
+		FROM parcels 
+		LEFT JOIN users AS sender ON parcels.sender_id = sender.id 
+		LEFT JOIN users AS recipient ON parcels.recipient_id = recipient.id 
+		LEFT JOIN cabinets ON parcels.id = cabinets.parcel_id
+		LEFT JOIN locations ON cabinets.location_id = locations.id
+		WHERE parcels.sending_code = $1 OR parcels.pickup_code = $1`;
+
+	const result: QueryResult<Parcel> = await pool.query(query, [code]);
 	return result.rows[0] || null;
 };
 
-const getByPickupCode = async (pickupCode: string): Promise<Parcel | null> => {
-	const query = 'SELECT id from parcels WHERE pickup_code = $1'
-	const result: QueryResult<Parcel> = await pool.query(query, [pickupCode]);
+const getBySendingCode = async (
+	sendingCode: string | null,
+): Promise<Parcel | null> => {
+	const query =
+		'SELECT id from parcels	WHERE sending_code = $1 AND parcel_status IS NULL';
+	const result: QueryResult<Parcel> = await pool.query(query, [sendingCode]);
 
+	return result.rows[0] || null;
+};
+
+const getByPickupCode = async (
+	pickupCode: string | null,
+): Promise<Parcel | null> => {
+	const query = `SELECT id from parcels	WHERE pickup_code = $1 AND parcel_status = 'ready-for-pickup'`;
+	const result: QueryResult<Parcel> = await pool.query(query, [pickupCode]);
 	return result.rows[0] || null;
 };
 
 const getParcelById = async (parcelId: string): Promise<Parcel | null> => {
 	const query = `
 		SELECT 
-			parcels.id, sending_code parcel_weight, special_instructions, parcel_size, CONCAT(sender.first_name,' ',sender.last_name) AS sender_full_name, CONCAT(recipient.first_name,' ',recipient.last_name) AS recipient_full_name, parcel_status, ready_for_pickup_at, parcels.updated_at, parcels.created_at,
+			parcels.id, sending_code, pickup_code, parcel_weight, special_instructions, parcel_size, CONCAT(sender.first_name,' ',sender.last_name) AS sender_full_name, CONCAT(recipient.first_name,' ',recipient.last_name) AS recipient_full_name, parcel_status, ready_for_pickup_at, parcels.updated_at, parcels.created_at,
 			CASE
 				WHEN cabinets.parcel_id IS NOT NULL THEN row_to_json(locations)
 				ELSE NULL
@@ -100,6 +118,23 @@ const updateParcelById = async (
 	return result.rows[0] || null;
 };
 
+const updateParcelNoUserId = async (
+	parcelId: string,
+	parcel: Partial<Parcel>,
+): Promise<Parcel | null> => {
+	const query =
+		'UPDATE parcels SET parcel_status = $1, driver_id = $2, pickup_code = $3, ready_for_pickup_at = $4 WHERE id = $5 RETURNING *';
+	const result: QueryResult<Parcel> = await pool.query(query, [
+		parcel.parcel_status,
+		parcel.driver_id || null,
+		parcel.pickup_code || null,
+		parcel.ready_for_pickup_at || null,
+		parcelId,
+	]);
+	console.log(result.rows[0]);
+	return result.rows[0] || null;
+}
+
 const deleteParcelById = async (parcelId: string): Promise<Parcel | null> => {
 	const query = 'DELETE FROM parcels WHERE id = $1 RETURNING *';
 	const result: QueryResult<Parcel> = await pool.query(query, [parcelId]);
@@ -125,6 +160,7 @@ const getAllParcels = async (): Promise<Parcel[] | []> => {
 
 export const parcelModel = {
 	getAllParcels,
+	getParcelByCode,
 	getBySendingCode,
 	getByPickupCode,
 	getParcelById,
@@ -132,5 +168,6 @@ export const parcelModel = {
 	getParcelsByDriverId,
 	create,
 	updateParcelById,
+	updateParcelNoUserId,
 	deleteParcelById,
 };
